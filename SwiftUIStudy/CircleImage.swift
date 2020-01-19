899ab878b50c4969b6c2dd48fae0ad58c1261194
liujun <1035841713@qq.com>
2020-01-19 15:19:16 +0800

    first commit

---
+++ SwiftUIStudy/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  SwiftUIStudy
+//
+//  Created by apple on 2020/1/17.
+//  Copyright © 2020 yinhe. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("test")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.red, lineWidth: 4)
+        )
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ SwiftUIStudy/MapView.swift
@@ -0,0 +1,32 @@
+//
+//  MapView.swift
+//  SwiftUIStudy
+//
+//  Created by apple on 2020/1/19.
+//  Copyright © 2020 yinhe. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    typealias UIViewType = MKMapView
+    
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        return MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
